b1ac749f2b20a9ed5d77adba19a3cef17be468df
souravbhowmik1999 <sourav.bhowmik@tekditechnologies.com>
2023-08-24 13:46:36 +0530

    Basic login registration

---
+++ Vriddhi_CMS/cms/views.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+import json
+from django.http import HttpResponse
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        User.objects.create_user(username=username, password=password)
+        return redirect('login')
+
+    return render(request, 'register.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    
+    return render(request, 'login.html')
+
+def home(request):
+    return render(request, 'home.html')
+
