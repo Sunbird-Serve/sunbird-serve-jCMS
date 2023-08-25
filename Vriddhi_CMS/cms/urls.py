1d813b874e3d9aef219c35144a3b91e8de92338f
souravbhowmik1999 <sourav.bhowmik@tekditechnologies.com>
2023-08-25 08:19:49 +0530

    add

--- Vriddhi_CMS/cms/urls.py
+++ Vriddhi_CMS/cms/urls.py
@@ -5,6 +5,8 @@ urlpatterns = patterns('cms.views',
     url(r'^$', 'user_login', name='login'),
     url(r'^register/$', 'register', name='register'),
     url(r'^home/$', 'home', name='home'),
+    url(r'^get_courses/$', 'get_courses', name='get_courses'),
+    url(r'^get_topics/$', 'get_topics', name='get_topics'),
 )
 
 (r'^static/admin/(?P<path>.*)$', 'django.views.static.serve',
