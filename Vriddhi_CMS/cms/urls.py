6be82b090fbef7d88226ffc2de44e304424a456e
souravbhowmik1999 <sourav.bhowmik@tekditechnologies.com>
2023-08-28 13:19:44 +0530

    add

--- Vriddhi_CMS/cms/urls.py
+++ Vriddhi_CMS/cms/urls.py
@@ -1,17 +1,18 @@
-from django.conf.urls import patterns, url
-from django.conf import settings
+from django.conf.urls import patterns
+from django.conf.urls import url
+from .views import *
+urlpatterns = [
+    url(r'^$', user_login, name='login'),
+    url(r'^register/$', register, name='register'),
+    url(r'^home/$', home, name='home'),
+    url(r'^get_courses/$', get_courses, name='get_courses'),
+    url(r'^get_topics/?$', get_topics, name='get_topics'),
+    url(r'^home/view_course/?$', view_course, name='view_course'),
+    url(r'^home/view_content/?$', view_content, name='view_content'),
+    url(r'^logout/$', logout_view, name='logout'),
+    url(r'^get_filtered_courses/$', get_filtered_courses, name='get_filtered_courses'),
+    # url(r'^get_filtered_subtopic/$', get_filtered_subtopic, name='get_filtered_subtopic'),
+    
+]
 
-urlpatterns = patterns('cms.views',
-    url(r'^$', 'user_login', name='login'),
-    url(r'^register/$', 'register', name='register'),
-    url(r'^home/$', 'home', name='home'),
-    url(r'^get_courses/$', 'get_courses', name='get_courses'),
-    url(r'^get_topics/$', 'get_topics', name='get_topics'),
-    url(r'^home/view_course/$', 'view_course', name='view_course'),
-)
 
-(r'^static/admin/(?P<path>.*)$', 'django.views.static.serve',
-               {'document_root': settings.ADMIN_MEDIA_PREFIX})
-
-(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-       'document_root': settings.STATIC_ROOT})
--- Vriddhi_CMS/cms/views.py
+++ Vriddhi_CMS/cms/views.py
@@ -3,8 +3,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 import json
 from django.http import HttpResponse
-from cms.models import Course
-from cms.models import Topic
+from cms.models import *
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.core import serializers
+# from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def register(request):
     if request.method == 'POST':
@@ -44,5 +49,81 @@ def get_topics(request):
     return HttpResponse(response_data, content_type="application/json")
 
 def view_course(request):
+    get_all_board = Board.objects.all()
     get_course_details = Course.objects.all()
-    return render(request, 'course_view.html',{'get_course_details': get_course_details})
+    return render(request, 'course_view.html',{'get_all_board':get_all_board,'get_course_details': get_course_details})
+
+
+
+
+
+
+
+
+def view_course(request):
+    get_all_board = Board.objects.all()
+    get_course_details = Course.objects.all()
+
+    # Number of items per page
+    items_per_page = 10  # You can adjust this according to your preference
+
+    paginator = Paginator(get_course_details, items_per_page)
+    page = request.GET.get('page')
+
+    try:
+        courses = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        courses = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        courses = paginator.page(paginator.num_pages)
+
+    return render(request, 'course_view.html', {
+        'get_all_board': get_all_board,
+        'courses': courses,
+    })
+
+
+def get_filtered_courses(request):
+    selected_board = request.GET.get('boardId')
+    if selected_board == 'all':
+        courses = Course.objects.all()
+        course_data = [{"id": course.id, "subject": course.subject.subject_name, "board": course.board.board_name, "grade": course.grade} for course in courses]
+    else:
+        courses = Course.objects.filter(board_id=selected_board)
+        course_data = [{"id": course.id, "subject": course.subject.subject_name, "board": course.board.board_name, "grade": course.grade} for course in courses]
+    response_data = json.dumps(course_data)
+    return HttpResponse(response_data, content_type="application/json")
+
+
+def view_content(request):
+    get_all_topic = Topic.objects.all()
+    get_all_subtopic = SubTopics.objects.all()
+    # get_content = SubTopics.ContentDetail.objects.all()
+    return render(request, 'content_view.html',{'get_all_topic':get_all_topic, 'get_all_subtopic': get_all_subtopic})
+
+# def get_filtered_subtopic(request):
+#     selected_topic = request.GET.get('topicId')
+#     if selected_topic == 'all':
+#         subtopics = SubTopic.objects.all()
+#         subtopics_data = [{"id": subtopic.id, "title": subtopic.title } for subtopic in subtopics]
+#     else:
+#         subtopics = SubTopic.objects.filter(board_id=selected_topic)
+#         subtopics_data = [{"id": subtopic.id, "title": subtopic.title } for subtopic in subtopics]
+
+#     response_data = json.dumps(subtopics_data)
+#     return HttpResponse(response_data, content_type="application/json")
+
+
+
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('')  
+
+
+# @login_required
+# def protected_view(request):
+#     return render(request, 'protected_template.html')
