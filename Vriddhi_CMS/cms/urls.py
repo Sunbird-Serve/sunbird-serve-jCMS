f34df71861d5df47f025d50f8827233d39a0c52e
souravbhowmik1999 <sourav.bhowmik@tekditechnologies.com>
2023-09-06 13:01:42 +0530

    commented code change

--- Vriddhi_CMS/cms/urls.py
+++ Vriddhi_CMS/cms/urls.py
@@ -9,14 +9,11 @@ urlpatterns = [
     url(r'^get_courses/$', get_courses, name='get_courses'),
     url(r'^get_topics/?$', get_topics, name='get_topics'),
     url(r'^home/view_course/?$', view_course, name='view_course'),
-    # url(r'^home/view_content/?$', view_content, name='view_content'),
     url(r'^home/view_content/$', view_content, name='view_content'),
     url(r'^home/content_detail/$', content_detail_view, name='content_detail'),
     url(r'^home/getSubtopic/?$', getSubtopic, name='getSubtopic'),
     url(r'^subtopic/content-details/?$', SubTopicDetailsView.as_view(), name="SubTopicDetailsView"),
     url(r'^subtopic/content_rating/$', content_rating, name='content_rating'),
-
-
     url(r'^logout/$', logout_view, name='logout'),
     url(r'^get_filtered_courses/$', get_filtered_courses, name='get_filtered_courses'),
     url(r'^get_filtered_subject/$', get_filtered_subject, name='get_filtered_subject'),
