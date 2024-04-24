e2963fc0f6863cde8932820ba889714e0da4d480
Rohan <rohan.jacob@digital.datamatics.com>
2024-04-24 11:52:59 +0530

    upgraded python

--- Vriddhi_CMS/settings.py
+++ Vriddhi_CMS/settings.py
@@ -1,10 +1,12 @@
 # Django settings for jcms project.
 import os
+from pathlib import Path
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 from local_settings import *
 # PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
-
+BASE_DIR = Path(__file__).resolve().parent.parent
+PROJECT_DIR = BASE_DIR
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
@@ -31,7 +33,7 @@ TIME_ZONE = 'America/Chicago'
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
@@ -51,18 +53,20 @@ MEDIA_ROOT = ''
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
-
+CORS_ORIGIN_ALLOW_ALL = True
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'Vriddhi_CMS','static'),  # Assuming you have a 'static' folder at the same level as 'manage.py'
+)
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # List of finder classes that know how to find static files in
 # various locations.
@@ -76,7 +80,21 @@ STATICFILES_FINDERS = (
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Static files configuration
-
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],  # You can specify directories here that Django will check for templates in.
+        'APP_DIRS': True,  # Allows template loaders to find templates in application directories.
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 
 # Make this unique, and don't share it with anybody.
@@ -92,7 +110,8 @@ TEMPLATE_LOADERS = (
 # Middleware
 
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
